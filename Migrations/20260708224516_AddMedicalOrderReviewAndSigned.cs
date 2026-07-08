b10f2afbeb250661c4a1a39207c048cfb7cd65b7
nes-ro <U202317099@upc.edu.pe>
2026-07-08 17:59:01 -0500

    fix: some bugs of authentication

---
+++ Migrations/20260708224516_AddMedicalOrderReviewAndSigned.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VitaliaBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMedicalOrderReviewAndSigned : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "review",
+                table: "medical_orders",
+                type: "varchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "signed",
+                table: "medical_orders",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "review",
+                table: "medical_orders");
+
+            migrationBuilder.DropColumn(
+                name: "signed",
+                table: "medical_orders");
+        }
+    }
+}
